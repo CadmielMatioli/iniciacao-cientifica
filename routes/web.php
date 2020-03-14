b1249482a7cdb9131b43e7c47cddb24013700932
cadmiel <cadmatioli@gmail.com>
2020-03-13 21:18:24 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 Route::post('sendmsg/mqtt/', 'MqttController@SendMsgViaMqtt')->name('sendmsg.mqtt');
 Route::get('subcribe/mqtt/{topic}', 'MqttController@SubscribetoTopic')->name('subscribe.mqtt');
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/subscribe', 'HomeController@topicSubscribeView')->name('subscribe.view');
+Route::get('home', 'HomeController@index')->name('home');
+Route::get('subscribe', 'HomeController@topicSubscribeView')->name('subscribe.view');
